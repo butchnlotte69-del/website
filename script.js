5c742af2c871ca56bebbb3debb18ed1b16b96d7a
butchnlotte69-del <butchnlotte69@gmail.com>
2026-09-03 13:37:59 -0500

    Add CryptoX script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+function getStarted() {
+  alert("Welcome to CryptoX! This is a demo project.");
+}
+
+function showMarkets() {
+  document.getElementById("markets").scrollIntoView({
+    behavior: "smooth"
+  });
+}
